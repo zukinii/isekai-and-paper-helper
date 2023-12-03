3d788d17f865176b1da6886818ba4ca478b87cd2
D <17290358+zukinii@users.noreply.github.com>
2023-12-03 04:10:52 +0100

    added dice sounds

---
+++ README.md
@@ -0,0 +1,25 @@
+# install instructions
+
+## how to add extension in firefox
+
+- open about:debugging#/runtime/this-firefox
+- click on load temporary add-on
+- select manifest.json file
+
+## how to add extension in chrome
+
+- open chrome://extensions/
+- click on load unpacked
+- select the folder
+
+## wie fügt man die Erweiterung in firefox hinzu
+
+- Öffne about:debugging#/runtime/this-firefox
+- Klicke auf "Temporäre Add-ons laden"
+- Wähle die manifest.json-Datei aus
+
+## wie fügt man die Erweiterung in chrome hinzu
+
+- Öffne chrome://extensions/
+- Klicke auf "Entpackte Erweiterung laden"
+- Wähle den Ordner aus
---
+++ content.js
@@ -0,0 +1,49 @@
+const addActionsToDrawings = async () => {
+	// wait some time until the drawings are loaded
+	await new Promise((resolve) => setTimeout(resolve, 5000))
+
+	const drawings = document.querySelectorAll('.waffle-borderless-embedded-object-overlay')
+	drawings.forEach((drawing) => drawing.addEventListener('click', onDrawingClick))
+}
+
+const onDrawingClick = (e) => {
+	const drawing = e.currentTarget
+
+	// sounds from https://soundbible.com/tags-rolling-dice.html
+	const allSounds = ['audio/dice1.mp3', 'audio/dice2.mp3', 'audio/dice-redneck.mp3']
+
+	// if the aria-label contains the value "würfelsound" then play the audio file
+	if (drawing.getAttribute('aria-label').toLowerCase().includes('würfelsound')) {
+		// play a random sound. make dic1 and dice2 evenly likely (50% each) and dice-redneck less likely (5%)
+		const random = Math.random()
+		if (random < 0.5) {
+			playAudio(allSounds[0])
+		} else if (random < 0.95) {
+			playAudio(allSounds[1])
+		} else {
+			playAudio(allSounds[2])
+		}
+	}
+}
+
+const getRuntimeURL = (file) => {
+	if (chrome) {
+		return chrome.runtime.getURL(file)
+	} else if (browser) {
+		return browser.runtime.getURL(file)
+	} else {
+		// Handle other cases or provide a default value
+		return file
+	}
+}
+
+const playAudio = (file) => {
+	const audio = new Audio(getRuntimeURL(file))
+	audio.play()
+}
+
+const init = () => {
+	addActionsToDrawings()
+}
+
+init()
